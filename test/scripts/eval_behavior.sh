3271b758b454457cef2e9caca6f63d9566a6e6f5
Daniel-xsy <1491387884@qq.com>
2024-10-28 10:20:13 -0700

    [add] eval script

--- test/scripts/eval_behavior.sh
+++ test/scripts/eval_behavior.sh
@@ -4,7 +4,7 @@ PREFIX=$2
 # Loop over all files in $PRED directory that start with $PREFIX
 for file in ${PRED}/${PREFIX}*; do
     # Run the evaluation command on each file
-    python evaluate/eval_percept.py \
+    python evaluate/eval_behavior.py \
         "$file" \
         ../data/QA_dataset_nus/drivelm_val_norm_final.json \
         --thresh 0.05
