e34d287b6432ac33226cc9d4d01c5eccb4887763
Daniel-xsy <1491387884@qq.com>
2024-10-29 21:49:58 -0700

    [update] video corruption config

--- corruptions/config_video.py
+++ corruptions/config_video.py
@@ -1,9 +1,9 @@
-json_file = 'data/QA_dataset_nus/v1_1_val_nus_q_only.json'
-data_root = './data/val_data/'
-corruption_root = './data/val_data_corruption'
+json_file = './data/QA_dataset_nus/drivelm_train_300_final_v2_norm.json'
+data_root = './data/nuscenes/samples/'
+corruption_root = './data/train_data_corruption'
 seed = 0
 
 img_norm_cfg = dict(mean=[0.0, 0.0, 0.0], std=[1.0, 1.0, 1.0], to_rgb=True)
 # delete easy and hard, change mid key to seberity
-# corruptions = [dict(type='H256ABRCompression', severity=4)]
-corruptions = [dict(type='BitError', severity=1)]
+corruptions = [dict(type='H256ABRCompression', severity=4),
+               dict(type='BitError', severity=2]
